8014b9f82d16caca858c97a2a7fdfffe021ed79c
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-01-29 15:18:37 +0600

    created a basic project

---
+++ java-text-editor/src/main/java/com/mycompany/java/text/editor/JavaTextEditor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.text.editor;
+
+/**
+ *
+ * @author ali43
+ */
+public class JavaTextEditor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
